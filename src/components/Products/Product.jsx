ece7134e0eb2602abd809a99ab83ced128501b1c
brz95 <abuizidov@bk.ru>
2022-12-02 23:05:25 +0300

    fixes

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,38 +1,36 @@
-import React, {useState} from 'react';
-import style from "./products.module.css"
-import {useDispatch} from "react-redux";
+import React from "react";
+import style from "./products.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
-const Product = ({image, name, price, left, discount,id}) => {
-const dispatch = useDispatch()
- const [amount,setAmount]=useState(0)
+const Product = ({ image, name, price, left, discount, id }) => {
+  const cartItems = useSelector((state) => state.cartItems);
+  const dispatch = useDispatch();
 
-const addCart = () =>{
-  setAmount(amount+1)
-  dispatch({type:"addCart", payload:{
-    productId:id,
-    amountCard:amount,
-    }})
-}
+  const addCart = () => {
+    dispatch({
+      type: "addCart",
+      payload: { id: cartItems.length + 1, productId: id, amount: 1 },
+    });
+  };
+
   return (
     <div className={style.product}>
       <div className={style.inner}>
-    <div className={style.image}>
-      <img src={image} alt="..."/>
-    </div>
-    <div >
-      <h1 className={style.name}>{name}</h1>
-    </div>
-      <div className={style.price}>
-        <h4>{`Цена: ${price}`}</h4>
-      </div>
-      <div>
-        {`В наличии: ${left} шт.`}
-      </div>
-      <div>
-        {discount===0 ? null : `Скидка: ${discount}%` }
-      </div>
+        <div className={style.image}>
+          <img src={image} alt="..." />
+        </div>
+        <div>
+          <h1 className={style.name}>{name}</h1>
+        </div>
+        <div className={style.price}>
+          <h4>{`Цена: ${price}`}</h4>
+        </div>
+        <div>{`В наличии: ${left} шт.`}</div>
+        <div>{discount === 0 ? null : `Скидка: ${discount}%`}</div>
         <div className={style.btn_buy}>
-        <button name="btn" onClick={addCart}> Купить </button>
+          <button name="btn" onClick={() => addCart()}>
+            Купить
+          </button>
         </div>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
-import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   // вывод списка категорий
-  const sideState = useSelector((initialState)=>initialState)
+  const sideState = useSelector((initialState) => initialState);
 
-  return sideState.categories.map((el)=> {return(<Link to={`category/${el.id}`}> <div>{el.name}</div></Link>)})
+  return sideState.categories.map((el) => {
+    return (
+      <Link to={`category/${el.id}`}>
+        {" "}
+        <div>{el.name}</div>
+      </Link>
+    );
+  });
 };
 
 export default Sidebar;
